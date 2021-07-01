bb78b3adf7ca6ac2e8996394d1ac7c28c1fdebc3
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-07-01 10:42:29 -0300

    primeiras metade de js feito

--- script/scripts.js
+++ script/scripts.js
@@ -4,27 +4,44 @@ const obj_botoes_array = document.querySelectorAll('.botoes')
 const obj_bt_calcular = document.querySelector('#bt_calcular')
 const obj_bt_limpar = document.querySelector('#bt_limpar')
 
-let limpeza = false
-let digitos = 0
+let limpeza_display = false
+let conta_digitos = 0
 
 for (obj_bt_num of obj_bt_num_array){
     obj_bt_num.addEventListener('click', function() { FunMontaDisplay(this.innerText) } )
 }
-
 for (obj_botoes of obj_botoes_array){
     const operacao = obj_botoes.getAttribute('data-oper')
     obj_botoes.addEventListener('click', function(){FunMontaDisplay(operacao)})
 }
+
 obj_bt_calcular.addEventListener('click', FunCalculaResultado)
 obj_bt_limpar.addEventListener('click', FunLimpaDisplay)
 
-FunMontaDisplay(par_innerText){
-    if (limpeza != false){
-        obj_txt_display.
+function FunMontaDisplay(par_txt){
+    if (limpeza_display == true){
+        obj_txt_display.value = ''
+        limpeza_display = false
+        conta_digitos = 0
     }
+    if (conta_digitos < 20){
+        obj_txt_display.value += par_txt
+        conta_digitos++
+    }else {
+        alert('LIMITE DE DIGITOS!')
+    }
+} // função monta display finalizada. numeros e sinais de operação adcionados.
+
+function FunCalculaResultado(){
+    try {
+        obj_txt_display.value = eval(obj_txt_display.value)
+    }catch(erro) {
+        alert('conta inválida. Erro: '+ erro.message)
+    }limpeza_display = true
+} // fim da função de calcular resultado com tratamento de erro
+
+function FunLimpaDisplay(){
+    obj_txt_display.value = ' '
 }
 
 
-/* for (obj_img of obj_figuras_array) { // como o primeiro objeto é um array, serás necessario criar um laço de repetição que ira ficar percorrendo o objeto e todas os seus elementos, tambem irei apelidar ele durante o processo
-    obj_img.addEventListener('click', function () { FunAdicionaFigura(this.src, this.alt) }) // aqui usando o array apelidado, criei um evento chamado click e ja atribui parametros a sua função
-}*/
